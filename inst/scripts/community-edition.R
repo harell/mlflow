ab3f15babe4b853940f58332eae3829bc4f1fca9
harell <harel@loostiger.com>
2021-02-11 14:16:37 +1300

    mlflow --> mockflow

--- inst/scripts/community-edition.R
+++ inst/scripts/community-edition.R
@@ -1,7 +1,9 @@
 # Setup -------------------------------------------------------------------
 pkgload::load_all()
 
+remotes::install_cran("mlflow")
 # usethis::edit_r_environ("project")
 # MLFLOW_TRACKING_URI=databricks
 # DATABRICKS_USERNAME=your@email.com
 # DATABRICKS_PASSWORD=1234
+
--- tests/testthat.R
+++ tests/testthat.R
@@ -1,4 +1,4 @@
 library(testthat)
-library(mlflow)
+library(mockflow)
 
-test_check("mlflow")
+test_check("mockflow")
