b0d066b1c4947e50efd64201cd4ee3d9134bdcb4
harell <harel@loostiger.com>
2021-02-22 14:51:24 +1300

    update scripts

--- inst/scripts/local-server.R
+++ inst/scripts/local-server.R
@@ -1,20 +1,8 @@
 # Setup -------------------------------------------------------------------
 pkgload::load_all()
-withr::with_envvar(list(
-    R_CONFIG_ACTIVE = "databricks",
-    R_CONFIG_FILE = system.file("inst", "mlflow.yml", package = pkgload::pkg_name(), mustWork = TRUE)
-), {
-    args <- config::get()
-    for(i in seq_along(args))
-        eval(parse(text = paste0("Sys.setenv(",names(args)[i],"='",args[[i]],"')")))
-})
-
 
 remotes::install_cran("mlflow")
-# usethis::edit_r_environ("project")
-# MLFLOW_TRACKING_URI=databricks
-# DATABRICKS_USERNAME=your@email.com
-# DATABRICKS_PASSWORD=1234
+
 
 # https://docs.databricks.com/dev-tools/api/latest/authentication.html
 
